a8fddba5d6a39da66066b95c870dcc029ce7b99a
Nakyanzi Catherine <n84252928@china.huawei.com>
2024-02-26 12:30:50 +0300

    quote

---
+++ forms/send-quote.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $company = $_POST['company'];
+    $location = $_POST['location'];
+    $service = $_POST['service'];
+    $details = $_POST['details'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    // Here you can process the form data, such as sending an email to Kemgro with the submitted details.
+    // Example:
+    $to = 'cnakyanzi2019@gmail.com'; // Change this to your email address
+    $subject = 'Quote Request from ' . $name;
+    $message = "Company: $company\nLocation: $location\nService Interested In: $service\nAdditional Details: $details\nName: $name\nEmail: $email\nPhone: $phone";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+
+    // Redirect to a thank you page
+  header("Location: index.html");
+  exit();
+}
+?>
